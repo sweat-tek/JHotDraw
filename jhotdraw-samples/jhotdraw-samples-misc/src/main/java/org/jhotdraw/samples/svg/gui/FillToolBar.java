6f2b9acb774d18cb2c7d7167d99d28fbb81fe89e
derReloader <62664722+derReloader@users.noreply.github.com>
2023-09-27 10:24:30 +0200

    - Remove feature @FeatureEntryPoint in ButtonFactory and FillToolBar

--- jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/svg/gui/FillToolBar.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/svg/gui/FillToolBar.java
@@ -7,7 +7,6 @@
  */
 package org.jhotdraw.samples.svg.gui;
 
-import dk.sdu.mmmi.featuretracer.lib.FeatureEntryPoint;
 import org.jhotdraw.gui.action.ButtonFactory;
 import org.jhotdraw.gui.plaf.palette.PaletteFormattedTextFieldUI;
 import org.jhotdraw.gui.plaf.palette.PaletteButtonUI;
@@ -66,7 +65,6 @@ public class FillToolBar extends AbstractToolBar {
         }
     }
 
-    @FeatureEntryPoint("FillToolBar.createDisclosedComponent")
     @Override
     protected JComponent createDisclosedComponent(int state) {
         JPanel p = null;
