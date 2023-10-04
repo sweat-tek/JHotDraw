0f0dfddfd1305a25436a3171e9c3732401426fe5
Rasmus Christiansen <rasm8024@edu.ucl.dk>
2023-10-04 09:26:22 +0200

    added redo and undo featurerace

--- jhotdraw-utils/src/main/java/org/jhotdraw/undo/UndoRedoManager.java
+++ jhotdraw-utils/src/main/java/org/jhotdraw/undo/UndoRedoManager.java
@@ -12,6 +12,8 @@ import java.beans.*;
 import java.util.*;
 import javax.swing.*;
 import javax.swing.undo.*;
+
+import dk.sdu.mmmi.featuretracer.lib.FeatureEntryPoint;
 import org.jhotdraw.util.*;
 
 /**
@@ -252,6 +254,7 @@ public class UndoRedoManager extends UndoManager { //javax.swing.undo.UndoManage
      * The UndoRedoManager ignores all incoming UndoableEdit events,
      * while undo is in progress.
      */
+    @FeatureEntryPoint(value="undo")
     @Override
     public void undo()
             throws CannotUndoException {
@@ -269,6 +272,7 @@ public class UndoRedoManager extends UndoManager { //javax.swing.undo.UndoManage
      * The UndoRedoManager ignores all incoming UndoableEdit events,
      * while redo is in progress.
      */
+    @FeatureEntryPoint(value="redo")
     @Override
     public void redo()
             throws CannotUndoException {
