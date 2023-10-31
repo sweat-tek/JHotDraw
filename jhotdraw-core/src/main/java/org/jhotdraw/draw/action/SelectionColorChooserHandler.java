4c8208611b3d6aacf53016795a02ca418fbef53e
derReloader <62664722+derReloader@users.noreply.github.com>
2023-10-31 22:18:31 +0100

    - Add refactoring ideas as comments to SelectionColorChooserHandler - Add unit test updateEnabledState() - Add unit tests for edge cases of applySelectedColorToFigures() - Add unit tests for edge cases of updateEnabledState()

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/action/SelectionColorChooserHandler.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/action/SelectionColorChooserHandler.java
@@ -29,12 +29,13 @@ public class SelectionColorChooserHandler extends AbstractSelectedAction
     private static final long serialVersionUID = 1L;
     protected AttributeKey<Color> key;
     protected JColorChooser colorChooser;
-    protected JPopupMenu popupMenu;
-    protected int isUpdating;
+    protected JPopupMenu popupMenu; // TODO should not care about popupMenu MVC, etc.
+    protected int isUpdating; // TODO Boolean?
 
+    // TODO Bad comments and commented code [LEVEL 1 Refactoring]
     //protected Map<AttributeKey, Object> attributes;
     /**
-     * Creates a new instance.
+     * Creates a new instance. TODO useless comment
      */
     public SelectionColorChooserHandler(DrawingEditor editor, AttributeKey<Color> key, JColorChooser colorChooser, JPopupMenu popupMenu) {
         super(editor);
@@ -42,36 +43,50 @@ public class SelectionColorChooserHandler extends AbstractSelectedAction
         this.colorChooser = colorChooser;
         this.popupMenu = popupMenu;
         //colorChooser.addActionListener(this);
-        colorChooser.getSelectionModel().addChangeListener(this);
+        colorChooser.getSelectionModel().addChangeListener(this); // TODO is anything achieved with using the change listener?
         updateEnabledState();
     }
 
     @Override
+    // TODO 2 entry points?! actionPerformed() and stateChanged()?
     public void actionPerformed(java.awt.event.ActionEvent evt) {
         /*
         if (evt.getActionCommand() == JColorChooser.APPROVE_SELECTION) {
             applySelectedColorToFigures();
         } else if (evt.getActionCommand() == JColorChooser.CANCEL_SELECTION) {
         }*/
+        // TODO class is about handling and applying colors, but should not care about menus?! [LEVEL 2 Refactoring]
         popupMenu.setVisible(false);
     }
 
+    // // TODO [CODE SMELL] Long Method <<< [LEVEL 1 Refactoring]
+    // Simplify method, split into multiple
     @FeatureEntryPoint("SelectionColorChooserHandler.applySelectedColorToFigures")
     protected void applySelectedColorToFigures() {
+        // TODO extract to class level?
         final ArrayList<Figure> selectedFigures = new ArrayList<>(getView().getSelectedFigures());
+        // TODO extract to class level?
         final ArrayList<Object> restoreData = new ArrayList<>(selectedFigures.size());
+        // TODO Guard - move to beginning if there is no color selected, don't proceed with the method
         Color selectedColor = colorChooser.getColor();
+        // TODO Maybe simplify boolean expression
         if (selectedColor != null && selectedColor.getAlpha() == 0) {
             selectedColor = null;
         }
         for (Figure figure : selectedFigures) {
+            // TODO different abstraction levels (figures, colors, events (?!)) - separate class? [LEVEL 2 REFACTORING]
             restoreData.add(figure.getAttributesRestoreData());
+            // TODO Observer pattern?
             figure.willChange();
             figure.set(key, selectedColor);
             figure.changed();
         }
         getEditor().setDefaultAttribute(key, selectedColor);
+        // TODO Shouldn't it be redo value?
         final Color undoValue = selectedColor;
+
+        // TODO refactor to be separate class
+        // TODO undo mechanism should be handled somewhere else [LEVEL 3 REFACTORING]
         UndoableEdit edit = new AbstractUndoableEdit() {
             private static final long serialVersionUID = 1L;
 
@@ -95,6 +110,7 @@ public class SelectionColorChooserHandler extends AbstractSelectedAction
                 super.undo();
                 Iterator<Object> iRestore = restoreData.iterator();
                 for (Figure figure : selectedFigures) {
+                    // TODO Code duplication?
                     figure.willChange();
                     figure.restoreAttributesTo(iRestore.next());
                     figure.changed();
@@ -105,6 +121,7 @@ public class SelectionColorChooserHandler extends AbstractSelectedAction
             public void redo() {
                 super.redo();
                 for (Figure figure : selectedFigures) {
+                    // TODO code duplication?
                     //restoreData.add(figure.getAttributesRestoreData());
                     figure.willChange();
                     figure.set(key, undoValue);
@@ -123,6 +140,7 @@ public class SelectionColorChooserHandler extends AbstractSelectedAction
             popupMenu.setEnabled(getView().getSelectionCount() > 0);
             isUpdating++;
             if (getView().getSelectionCount() > 0 /*&& colorChooser.isShowing()*/) {
+                // TODO Solarlint: Loops with at most one iteration should be refactored [LEVEL 2 REFACTORING]
                 for (Figure f : getView().getSelectedFigures()) {
                     Color figureColor = f.get(key);
                     colorChooser.setColor(figureColor == null ? new Color(0, true) : figureColor);
@@ -135,6 +153,7 @@ public class SelectionColorChooserHandler extends AbstractSelectedAction
 
     @Override
     public void stateChanged(ChangeEvent e) {
+        // TODO simplify contional expression // make isUpdating a boolean
         if (isUpdating++ == 0) {
             applySelectedColorToFigures();
         }
--- jhotdraw-core/src/test/java/org/jhotdraw/draw/action/SelectionColorChooserHandlerTest.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/SelectionColorChooserHandlerTest.java
@@ -2,69 +2,164 @@ package org.jhotdraw.draw.action;
 
 import org.jhotdraw.draw.*;
 import org.jhotdraw.draw.figure.Figure;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.swing.*;
 import javax.swing.colorchooser.ColorSelectionModel;
 import java.awt.*;
-import java.util.Set;
+import java.util.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class SelectionColorChooserHandlerTest {
 
-    SelectionColorChooserHandler handler;
     DrawingEditor editor;
-    AttributeKey<Color> key;
-    JColorChooser selectionChooser;
+    SelectionColorChooserHandler handler;
+    JColorChooser colorChooser;
     DrawingView drawingView;
+    JPopupMenu popupMenu;
 
     // TODO Report: Test setup very cumbersome
     @Before
     public void setUp() {
         editor = mock(DrawingEditor.class);
-        key = AttributeKeys.FILL_COLOR;
-        selectionChooser = mock(JColorChooser.class);
-        ColorSelectionModel selectionModel = mock(ColorSelectionModel.class);
-        when(selectionChooser.getSelectionModel()).thenReturn(selectionModel);
-        JPopupMenu menu = mock(JPopupMenu.class);
+        AttributeKey<Color> key = AttributeKeys.FILL_COLOR;
+        colorChooser = mock(JColorChooser.class);
+        when(colorChooser.getSelectionModel()).thenReturn(mock(ColorSelectionModel.class));
+        popupMenu = mock(JPopupMenu.class);
         handler = new SelectionColorChooserHandler(
                 editor,
                 key,
-                selectionChooser,
-                menu
+                colorChooser,
+                popupMenu
         );
         drawingView = mock(DrawingView.class);
         when(drawingView.getDrawing()).thenReturn(mock(Drawing.class));
         when(editor.getActiveView()).thenReturn(drawingView);
     }
 
-    @After
-    public void tearDown() {
-    }
-
-
-    // Mock that colorChooser returns red
-    // Check that figure.set is called with "fillColor" and red
-    // Mock response of figure.set to return red
-
     @Test
-    public void testApplyChosenColorToSelectedFigure() {
-        // given
+    public void testApplySelectedColorToFigures() {
+        // given a selected figure and the color chooser set to the color red
         Figure figure = mock(Figure.class);
-        when(drawingView.getSelectedFigures()).thenReturn(Set.of(figure));
-        when(selectionChooser.getColor()).thenReturn(Color.red);
+        when(drawingView.getSelectedFigures()).thenReturn(new HashSet<>(Collections.singletonList(figure)));
+        when(drawingView.getSelectionCount()).thenReturn(1);
+        when(colorChooser.getColor()).thenReturn(Color.red);
 
-        // when
+        // when the apply selected color to figures method is called
         handler.applySelectedColorToFigures();
 
-        // then
-        verify(selectionChooser, times(1)).getColor();
+        // then verify that the current color of the color chooser is fetched, and
+        // that the fetched color is applied to the selected figure, and
+        // that the default of the editor is set to the fetched color
+        verify(colorChooser, times(1)).getColor();
         verify(figure, times(1)).willChange();
         verify(figure, times(1)).set(AttributeKeys.FILL_COLOR, Color.red);
         verify(figure, times(1)).changed();
+        verify(editor, times(1)).setDefaultAttribute(AttributeKeys.FILL_COLOR, Color.red);
+    }
+
+    // TODO Report: This test shows that it might make sense to stop the method after fetching no color
+    // SelectionColorChooserHandler:71
+    @Test
+    public void testApplySelectedColorToFiguresNoSelectedColor() {
+        // given a selected figure and the color chooser set to no color
+        Figure figure = mock(Figure.class);
+        when(drawingView.getSelectedFigures()).thenReturn(new HashSet<>(Collections.singletonList(figure)));
+        when(drawingView.getSelectionCount()).thenReturn(1);
+        when(colorChooser.getColor()).thenReturn(null);
+
+        // when the apply selected color to figures method is called
+        handler.applySelectedColorToFigures();
+
+        // then verify that no color of the color chooser is fetched, and
+        // that no color is applied to the selected figure, and
+        // that the default of the editor is set to no color
+        verify(colorChooser, times(1)).getColor();
+        // TODO The method could already stop here, i.e. after receiving no color from the chooser
+        verify(figure, times(1)).willChange();
+        verify(figure, times(1)).set(AttributeKeys.FILL_COLOR, null);
+        verify(figure, times(1)).changed();
+        verify(editor, times(1)).setDefaultAttribute(AttributeKeys.FILL_COLOR, null);
+    }
+
+    @Test
+    public void testApplySelectedColorToFiguresNoFigureSelected() {
+        // given a selected figure and the color chooser set to no color
+        when(drawingView.getSelectedFigures()).thenReturn(Collections.emptySet());
+        when(drawingView.getSelectionCount()).thenReturn(0);
+        when(colorChooser.getColor()).thenReturn(Color.red);
+
+        // when the apply selected color to figures method is called
+        handler.applySelectedColorToFigures();
+
+        // then verify that the current color of the color chooser is fetched, and
+        // that no color is applied to the selected figure, and
+        // that the default of the editor is set to the fetched color
+        verify(colorChooser, times(1)).getColor();
+        verify(editor, times(1)).setDefaultAttribute(AttributeKeys.FILL_COLOR, Color.red);
+    }
+
+    @Test
+    public void testUpdateEnabledState() {
+        // given a selected figure
+        when(drawingView.getSelectedFigures()).thenReturn(new HashSet<>(Collections.singletonList(mock(Figure.class))));
+        when(drawingView.getSelectionCount()).thenReturn(1);
+
+        // when the update enabled state method is called
+        handler.updateEnabledState();
+
+        // then verify that color chooser and popup menu are updated to be enabled, and
+        // that the color chooser is set to the color of the selected figure if any
+        verify(colorChooser, times(1)).setEnabled(true);
+        verify(popupMenu, times(1)).setEnabled(true);
+        verify(colorChooser, times(1)).setColor(new Color(0, true));
+        assertEquals(0, handler.isUpdating);
+    }
+
+    @Test
+    public void testUpdateEnabledStateNoFigureSelected() {
+        // given no figures selected
+        when(drawingView.getSelectedFigures()).thenReturn(Collections.emptySet());
+        when(drawingView.getSelectionCount()).thenReturn(0);
+
+        // when the update enabled state method is called
+        handler.updateEnabledState();
+
+        // then verify that color chooser and popup menu are updated to be disabled, and
+        // that the color chooser is not set to the color of the selected figure
+        verify(colorChooser, times(1)).setEnabled(false);
+        verify(popupMenu, times(1)).setEnabled(false);
+        verify(colorChooser, times(0)).setColor(any());
+        assertEquals(0, handler.isUpdating);
+    }
+
+    @Test
+    public void testUpdateEnabledStateMultipleFiguresSelected() {
+        // given a multiple selected figures, one colorless and one colorful
+        Figure colorlessFigure = mock(Figure.class);
+        Figure colorfulFigure = mock(Figure.class);
+        when(colorfulFigure.get(AttributeKeys.FILL_COLOR)).thenReturn(Color.red);
+        // TODO Weak point of current impl:
+        // Based on the chosen set implementation it is somewhat random which figure is used for determining the color
+        // There should be some clear rule in place, e.g. use the color of the lastly selected figure
+        when(drawingView.getSelectedFigures())
+                .thenReturn(new LinkedHashSet<>(Arrays.asList(colorlessFigure, colorfulFigure)));
+        when(drawingView.getSelectionCount()).thenReturn(2);
+
+        // when the update enabled state method is called
+        handler.updateEnabledState();
+
+        // then verify that color chooser and popup menu are updated to be enabled, and
+        // that the color chooser is set to the color of first figure in the set
+        verify(colorChooser, times(1)).setEnabled(true);
+        verify(popupMenu, times(1)).setEnabled(true);
+        verify(colorChooser, times(1)).setColor(new Color(0, true));
+        // TODO Should be something like 'take value from last selected figure' after the refacoring
+        // verify(colorChooser, times(1)).setColor(Color.red);
+        assertEquals(0, handler.isUpdating);
     }
 
 }
