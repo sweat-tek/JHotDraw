ff14d280781ed825771b394a6d84da62083c0bf4
frmad <frmad21@student.sdu.dk>
2023-11-03 18:15:18 +0100

    Clean up for tests

--- jhotdraw-core/src/test/java/org/jhotdraw/draw/action/ArrangeActionTest2.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/ArrangeActionTest2.java
@@ -49,8 +49,7 @@ public class ArrangeActionTest2 {
 
         //other drawing
         view1 = mock(DrawingView.class);
-        //drawing = new QuadTreeDrawing();//for større drawings
-        drawing1 = new DefaultDrawing();//for mindre drawings
+        drawing1 = new DefaultDrawing();
 
         // create figures and add them to the drawing
         figure1 = new EllipseFigure();
@@ -81,9 +80,6 @@ public class ArrangeActionTest2 {
         drawing2.add(figure5);
         drawing2.add(figure4);
 
-        /*System.out.println(figure5 + " " + figure6 + " " + figure7 + " " + figure8);
-        System.out.println(d.getFiguresFrontToBack());
-        System.out.println(v.getDrawing());*/
 
         selectedFiguresMulti = new LinkedHashSet<>();
         selectedFiguresMulti.add(figure5);
@@ -117,117 +113,38 @@ public class ArrangeActionTest2 {
     public void arrangeFiguresBestCaseOneFigure() {
         //Bring to front
         testBringToFront(view, drawing, selectedFigure, figure, figure);
-        //figure in the front
-        /*Figure frontFigure = drawing.getFiguresFrontToBack().get(0);
-        assertSame(figure, frontFigure);
-
-        ArrangeAction.arrangeFigures(view, selectedFigure, ArrangeAction.BRING_TO_FRONT); //send figure to the front
-
-        //figure in the front
-        Figure frontFigureNew = drawing.getFiguresFrontToBack().get(0);
-        assertSame(figure, frontFigureNew);*/
 
         //Send to back
         testSendToBack(view, drawing, selectedFigure, figure, figure);
-        // figure at the back
-        /*Figure backFigure = drawing.getFiguresFrontToBack().get(drawing.getFiguresFrontToBack().size() - 1);
-        assertSame(figure, backFigure);
-
-        ArrangeAction.arrangeFigures(view, selectedFigure, ArrangeAction.SEND_TO_BACK); //send figure to the back
-
-        // figure at the back
-        Figure backFigureNew = drawing.getFiguresFrontToBack().get(drawing.getFiguresFrontToBack().size() - 1);
-        assertSame(figure, backFigureNew);*/
     }
 
     @Test
     public void arrangeFiguresBestCaseAlreadyArranged() {
         //Bring to front
-        //figure1 in the front
-        /*Figure frontFigure = drawing1.getFiguresFrontToBack().get(0);
-        assertSame(figure1, frontFigure);*/
 
         Set<Figure> selectedFigure1 = new LinkedHashSet<>();
         selectedFigure1.add(figure1);
         when(view1.getSelectedFigures()).thenReturn(selectedFigure1);
 
-        /*ArrangeAction.arrangeFigures(view1, selectedFigure1, ArrangeAction.BRING_TO_FRONT); //send figure to the front
-
-        //figure1 in the front
-        Figure frontFigureNew = drawing1.getFiguresFrontToBack().get(0);
-        assertSame(figure1, frontFigureNew);*/
-
         testBringToFront(view1, drawing1, selectedFigure1, figure1, figure1);
 
         //Send to back
-        // figure3 at the back
-        /*Figure backFigure = drawing1.getFiguresFrontToBack().get(drawing1.getFiguresFrontToBack().size() - 1);
-        assertSame(figure3, backFigure);*/
 
         Set<Figure> selectedFigure3 = new LinkedHashSet<>();
         selectedFigure1.add(figure3);
         when(view1.getSelectedFigures()).thenReturn(selectedFigure3);
 
-        /*ArrangeAction.arrangeFigures(view1, selectedFigure3, ArrangeAction.SEND_TO_BACK); //send figure to the back
-
-        // figure at the back
-        Figure backFigureNew = drawing1.getFiguresFrontToBack().get(drawing1.getFiguresFrontToBack().size() - 1);
-        assertSame(figure3, backFigureNew);*/
         testSendToBack(view1, drawing1, selectedFigure3, figure3, figure3);
-    }
-
-    @Test
-    public void getResourceBundle() {
     }
 
     @Test
     public void testArrangeFiguresBoundaryCaseBringToFront() {
         testBringToFront(view1, drawing1, selectedFigures, figure1, figure2);
-
-        //figure1 in the front
-        /*Figure frontFigure = drawing1.getFiguresFrontToBack().get(0);
-        assertSame(figure1, frontFigure);
-
-        ArrangeAction.arrangeFigures(view1, selectedFigures, ArrangeAction.BRING_TO_FRONT); //send figure2 to the front
-
-        //figure2 in the front
-        Figure frontFigureNew = drawing1.getFiguresFrontToBack().get(0);
-        assertSame(figure2, frontFigureNew);*/
-
-        //gammelt
-        /*// figure1 in the front
-        Figure frontFigure = drawing.getFiguresFrontToBack().iterator().next(); // Z-order
-        assertSame(figure1, frontFigure);
-
-        ArrangeAction.arrangeFigures(view, selectedFigures, ArrangeAction.BRING_TO_FRONT); //send figure2 to the front
-        //figure2 in the front
-        Figure frontFigureNew = drawing.getFiguresFrontToBack().iterator().next(); // Z-order
-        assertSame(figure2, frontFigureNew);*/
     }
 
     @Test
     public void testArrangeFiguresBoundaryCaseSendToBack() {
         testSendToBack(view1, drawing1, selectedFigures, figure3, figure2);
-        // figure3 at the back
-        /*Figure backFigure = drawing1.getFiguresFrontToBack().get(drawing1.getFiguresFrontToBack().size() - 1);
-        assertSame(figure3, backFigure);
-
-        ArrangeAction.arrangeFigures(view1, selectedFigures, ArrangeAction.SEND_TO_BACK); //send figure2 to the back
-
-        // figure2 at the back
-        Figure backFigureNew = drawing1.getFiguresFrontToBack().get(drawing1.getFiguresFrontToBack().size() - 1);
-        assertSame(figure2, backFigureNew);*/
-
-        //gammelt
-        /*// figure at the back
-        Figure backFigure = drawing.getFiguresFrontToBack().get(0); // Z-order, from back to front.
-        assertSame(figure1, backFigure);
-
-        ArrangeAction.arrangeFigures(view, selectedFigures, ArrangeAction.SEND_TO_BACK); //send figure2 to the back
-
-        //
-        Figure backFigureNew = drawing.getFiguresFrontToBack().get(0); // Z-order, from back to front.
-        assertSame(figure1, backFigureNew);*/
 
     }
 
@@ -245,6 +162,9 @@ public class ArrangeActionTest2 {
             assertSame(f, figuresFromTheDrawing.get(indexForFigure));
             indexForFigure++;
         }
+    }
+    @Test
+    public void getResourceBundle() {
     }
 
     private void testSendToBack(DrawingView view, Drawing drawing, Set<Figure> selectedFigures, Figure beforeArrange, Figure afterArrange){
