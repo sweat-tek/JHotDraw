f8c77e668df924af83b2b4848cda63d03d44a0ef
FrejaStrand <90042340+FrejaStrand@users.noreply.github.com>
2023-11-08 08:54:47 +0100

    Jgiven is now used for automating BDD scenarios

---
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/jgivenstages/GivenEllipseDrawing.java
@@ -0,0 +1,17 @@
+package org.jhotdraw.samples.svg.figures.jgivenstages;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import org.jhotdraw.draw.Drawing;
+import org.jhotdraw.draw.QuadTreeDrawing;
+
+public class GivenEllipseDrawing extends Stage<GivenEllipseDrawing> {
+
+    @ProvidedScenarioState
+    Drawing drawing;
+
+    public GivenEllipseDrawing the_user_has_selected_the_ellipse_drawing_tool() {
+        this.drawing = new QuadTreeDrawing();
+        return this;
+    }
+}
---
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/jgivenstages/ThenEllipseDrawing.java
@@ -0,0 +1,24 @@
+package org.jhotdraw.samples.svg.figures.jgivenstages;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import org.jhotdraw.draw.Drawing;
+import org.jhotdraw.draw.QuadTreeDrawing;
+import org.jhotdraw.draw.figure.Figure;
+import org.jhotdraw.samples.svg.figures.SVGEllipseFigure;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class ThenEllipseDrawing extends Stage<ThenEllipseDrawing> {
+
+    @ProvidedScenarioState
+    Drawing drawing;
+
+    public ThenEllipseDrawing an_ellipse_shape_should_be_created_with_the_specified_dimensions() {
+        List<Figure> figures = drawing.getFiguresFrontToBack();
+        assertTrue(figures.stream().anyMatch(figure -> figure instanceof SVGEllipseFigure));
+        return this;
+    }
+}
