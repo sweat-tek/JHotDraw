311467eec607a29ebe25f2e386edaa5fdc1605cf
frmad <frmad21@student.sdu.dk>
2023-11-16 17:05:20 +0100

    Update ArrangeActionTest2.java

--- jhotdraw-core/src/test/java/org/jhotdraw/draw/action/ArrangeActionTest2.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/ArrangeActionTest2.java
@@ -15,27 +15,26 @@ import static org.junit.Assert.*;
 
 public class ArrangeActionTest2 {
     private DrawingView view;
-    private Drawing drawing;
     private DrawingView view1;
-    private Drawing drawing1;
     private DrawingView view2;
+    private Drawing drawing;
+    private Drawing drawing1;
     private Drawing drawing2;
+    private Figure figure;
     private Figure figure1;
     private Figure figure2;
     private Figure figure3;
-
     private Figure figure4;
-
     private Figure figure5;
     private Figure figure6;
     private Figure figure7;
-    private Figure figure;
     private Set<Figure> selectedFigure;
     private Set<Figure> selectedFigures;
     private Set<Figure> selectedFiguresMulti;
 
     @Before
     public void setUp() {
+        //drawing
         view = mock(DrawingView.class);
         drawing = new DefaultDrawing();
         figure = new EllipseFigure();
@@ -45,13 +44,12 @@ public class ArrangeActionTest2 {
 
         selectedFigure = new LinkedHashSet<>();
         selectedFigure.add(figure);
+
         when(view.getSelectedFigures()).thenReturn(selectedFigure);
 
         //other drawing
         view1 = mock(DrawingView.class);
         drawing1 = new DefaultDrawing();
-
-        // create figures and add them to the drawing
         figure1 = new EllipseFigure();
         figure2 = new EllipseFigure();
         figure3 = new EllipseFigure();
@@ -63,27 +61,27 @@ public class ArrangeActionTest2 {
 
         selectedFigures = new LinkedHashSet<>();
         selectedFigures.add(figure2);
+
         when(view1.getSelectedFigures()).thenReturn(selectedFigures);
 
         //other drawing
         view2 = mock(DrawingView.class);
         drawing2 = new DefaultDrawing();
-        when(view2.getDrawing()).thenReturn(drawing2);
-
-        Figure figure4 = new EllipseFigure();
-        Figure figure5 = new EllipseFigure();
-        Figure figure6 = new EllipseFigure();
-        Figure figure7 = new EllipseFigure();
-
+        figure4 = new EllipseFigure();
+        figure5 = new EllipseFigure();
+        figure6 = new EllipseFigure();
+        figure7 = new EllipseFigure();
         drawing2.add(figure7);
         drawing2.add(figure6);
         drawing2.add(figure5);
         drawing2.add(figure4);
 
+        when(view2.getDrawing()).thenReturn(drawing2);
 
         selectedFiguresMulti = new LinkedHashSet<>();
         selectedFiguresMulti.add(figure5);
         selectedFiguresMulti.add(figure6);
+
         when(view2.getSelectedFigures()).thenReturn(selectedFiguresMulti);
     }
 
@@ -109,6 +107,7 @@ public class ArrangeActionTest2 {
     public void actionPerformed() {
     }
 
+    //Best cases
     @Test
     public void arrangeFiguresBestCaseOneFigure() {
         //Bring to front
@@ -121,7 +120,6 @@ public class ArrangeActionTest2 {
     @Test
     public void arrangeFiguresBestCaseAlreadyArranged() {
         //Bring to front
-
         Set<Figure> selectedFigure1 = new LinkedHashSet<>();
         selectedFigure1.add(figure1);
         when(view1.getSelectedFigures()).thenReturn(selectedFigure1);
@@ -129,47 +127,73 @@ public class ArrangeActionTest2 {
         testBringToFront(view1, drawing1, selectedFigure1, figure1, figure1);
 
         //Send to back
-
         Set<Figure> selectedFigure3 = new LinkedHashSet<>();
-        selectedFigure1.add(figure3);
+        selectedFigure3.add(figure3);
         when(view1.getSelectedFigures()).thenReturn(selectedFigure3);
 
         testSendToBack(view1, drawing1, selectedFigure3, figure3, figure3);
     }
 
+    //Boundary cases
     @Test
-    public void testArrangeFiguresBoundaryCaseBringToFront() {
+    public void arrangeFiguresOneFigureSelected() {
+        //Bring to front
         testBringToFront(view1, drawing1, selectedFigures, figure1, figure2);
-    }
 
-    @Test
-    public void testArrangeFiguresBoundaryCaseSendToBack() {
+        //Send to Back
         testSendToBack(view1, drawing1, selectedFigures, figure3, figure2);
-
     }
 
     @Test
-    public void testArrangeFiguresBoundaryCaseMultipleSelectedFigures() {
+    public void arrangeFiguresAllFiguresSelected() {
+        Set<Figure> selected = new LinkedHashSet<>();
+        selected.add(figure3);
+        selected.add(figure2);
+        selected.add(figure1);
+        when(view1.getSelectedFigures()).thenReturn(selected);
 
+        //Bring to front
+        testBringToFront(view1, drawing1, selected, figure1, figure1);
+
+        //Send to back
+        testSendToBack(view1, drawing1, selected, figure3, figure1);
+    }
+
+    @Test
+    public void arrangeFiguresMultipleSelectedBringToFront() {
         ArrangeAction.arrangeFigures(view2, selectedFiguresMulti, ArrangeAction.BRING_TO_FRONT);
 
-        // check if the figures 5 and 6 are in the front
         List<Figure> figuresFromTheDrawing = new ArrayList<>(drawing2.getFiguresFrontToBack());
         int indexForFigure = figuresFromTheDrawing.indexOf(selectedFiguresMulti.iterator().next());
 
-        //shows that figure 5 and 6 are in the front after the ArrangeAction
+        //shows that figure 5 and 6 now are in the front after the ArrangeAction
         for (Figure f : selectedFiguresMulti) {
             assertSame(f, figuresFromTheDrawing.get(indexForFigure));
             indexForFigure++;
         }
     }
     @Test
+    public void arrangeFiguresMultipleSelectedSendToBack() {
+        ArrangeAction.arrangeFigures(view2, selectedFiguresMulti, ArrangeAction.SEND_TO_BACK);
+
+        List<Figure> figuresFromTheDrawing = new ArrayList<>(drawing2.getFiguresFrontToBack());
+        int indexForFigure = figuresFromTheDrawing.size() - 1;
+
+        //shows that figure 5 and 6 now are at the back after the ArrangeAction
+        for (Figure f : selectedFiguresMulti) {
+            assertSame(f, figuresFromTheDrawing.get(indexForFigure));
+            indexForFigure--;
+
+        }
+    }
+
+    @Test
     public void getResourceBundle() {
     }
 
     private void testSendToBack(DrawingView view, Drawing drawing, Set<Figure> selectedFigures, Figure beforeArrange, Figure afterArrange){
         Figure backFigure = drawing.getFiguresFrontToBack().get(drawing.getFiguresFrontToBack().size() - 1);
-        assertSame(backFigure, backFigure);
+        assertSame(beforeArrange, backFigure);
 
         ArrangeAction.arrangeFigures(view, selectedFigures, ArrangeAction.SEND_TO_BACK);
 
