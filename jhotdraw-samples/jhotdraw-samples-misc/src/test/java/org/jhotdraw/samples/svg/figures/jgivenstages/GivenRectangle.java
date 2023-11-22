6fa13f40fea770adac2d2dc902f2b694e4597cf6
Siole <siole21@student.sdu.dk>
2023-11-22 10:08:05 +0100

    update on BDD test

--- jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/jgivenstages/GivenRectangle.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/jgivenstages/GivenRectangle.java
@@ -2,14 +2,13 @@ package org.jhotdraw.samples.svg.figures.jgivenstages;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
-import com.tngtech.jgiven.annotation.ScenarioState;
 import org.jhotdraw.draw.DefaultDrawing;
 import org.jhotdraw.draw.Drawing;
-import org.jhotdraw.draw.QuadTreeDrawing;
+
 
 public class GivenRectangle extends Stage<GivenRectangle>{
 
-    @ScenarioState
+    @ProvidedScenarioState
     Drawing drawing;
 
     public GivenRectangle empty_canvas(){
