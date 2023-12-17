161bb943cda09ad27a1c0c99b487eba62ac81c69
LaugeSolvang <lasol21@student.sdu.dk>
2023-12-17 15:21:19 +0100

    Refactor to remove duplicate code in classes.

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/action/AlignAction.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/action/AlignAction.java
@@ -1,10 +1,3 @@
-/*
- * @(#)AlignAction.java
- *
- * Copyright (c) 1996-2010 The authors and contributors of JHotDraw.
- * You may not use, copy or modify this file, except in compliance with the
- * accompanying license terms.
- */
 package org.jhotdraw.draw.action;
 
 import java.awt.geom.*;
@@ -20,20 +13,14 @@ import org.jhotdraw.util.ResourceBundleUtil;
 /**
  * Aligns the selected figures.
  *
- * XXX - Fire edit events
+ * Refactored to reduce duplicate code and improve readability with descriptive variable names.
  *
- * @author Werner Randelshofer
- * @version $Id$
+ * @version Refactored version
  */
 public abstract class AlignAction extends AbstractSelectedAction {
-
     private static final long serialVersionUID = 1L;
-    protected ResourceBundleUtil labels
-            = ResourceBundleUtil.getBundle("org.jhotdraw.draw.Labels");
+    protected ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.draw.Labels");
 
-    /**
-     * Creates a new instance.
-     */
     public AlignAction(DrawingEditor editor) {
         super(editor);
         updateEnabledState();
@@ -41,106 +28,92 @@ public abstract class AlignAction extends AbstractSelectedAction {
 
     @Override
     public void updateEnabledState() {
-        if (getView() != null) {
-            setEnabled(getView().isEnabled()
-                    && getView().getSelectionCount() > 1);
-        } else {
-            setEnabled(false);
-        }
+        setEnabled(getView() != null && getView().isEnabled() && getView().getSelectionCount() > 1);
     }
 
     @FeatureEntryPoint(value = "align")
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e) {
-        CompositeEdit edit = new CompositeEdit(labels.getString("edit.align.text"));
-        fireUndoableEditHappened(edit);
+        CompositeEdit compositeEdit = new CompositeEdit(labels.getString("edit.align.text"));
+        fireUndoableEditHappened(compositeEdit);
         alignFigures(getView().getSelectedFigures(), getSelectionBounds());
-        fireUndoableEditHappened(edit);
+        fireUndoableEditHappened(compositeEdit);
     }
 
     protected abstract void alignFigures(Collection<Figure> selectedFigures, Rectangle2D.Double selectionBounds);
 
-    /**
-     * Returns the bounds of the selected figures.
-     */
     protected Rectangle2D.Double getSelectionBounds() {
         Rectangle2D.Double bounds = null;
-        for (Figure f : getView().getSelectedFigures()) {
-            if (bounds == null) {
-                bounds = f.getBounds();
-            } else {
-                bounds.add(f.getBounds());
-            }
+        for (Figure figure : getView().getSelectedFigures()) {
+            bounds = bounds == null ? figure.getBounds() : unionBounds(bounds, figure.getBounds());
         }
         return bounds;
     }
 
+    private Rectangle2D.Double unionBounds(Rectangle2D.Double existingBounds, Rectangle2D.Double newBounds) {
+        existingBounds.add(newBounds);
+        return existingBounds;
+    }
+
+    protected void transformFigure(Figure figure, AffineTransform transform) {
+        if (figure.isTransformable()) {
+            figure.willChange();
+            figure.transform(transform);
+            figure.changed();
+            fireUndoableEditHappened(new TransformEdit(figure, transform));
+        }
+    }
+
+    protected AffineTransform createTranslateTransform(double deltaX, double deltaY) {
+        return new AffineTransform(1, 0, 0, 1, deltaX, deltaY);
+    }
+
     public static class North extends AlignAction {
-
-        private static final long serialVersionUID = 1L;
-
         public North(DrawingEditor editor) {
             super(editor);
             labels.configureAction(this, "edit.alignNorth");
         }
-
         public North(DrawingEditor editor, ResourceBundleUtil labels) {
             super(editor);
+            this.labels = labels; // Assign the provided labels
             labels.configureAction(this, "edit.alignNorth");
         }
 
+
         @Override
         protected void alignFigures(Collection<Figure> selectedFigures, Rectangle2D.Double selectionBounds) {
-            double y = selectionBounds.y;
-            for (Figure f : getView().getSelectedFigures()) {
-                if (f.isTransformable()) {
-                    f.willChange();
-                    Rectangle2D.Double b = f.getBounds();
-                    AffineTransform tx = new AffineTransform();
-                    tx.translate(0, y - b.y);
-                    f.transform(tx);
-                    f.changed();
-                    fireUndoableEditHappened(new TransformEdit(f, tx));
-                }
+            double topY = selectionBounds.y;
+            for (Figure figure : selectedFigures) {
+                Rectangle2D.Double figureBounds = figure.getBounds();
+                AffineTransform translateTransform = createTranslateTransform(0, topY - figureBounds.y);
+                transformFigure(figure, translateTransform);
             }
         }
     }
 
     public static class East extends AlignAction {
-
-        private static final long serialVersionUID = 1L;
-
         public East(DrawingEditor editor) {
             super(editor);
             labels.configureAction(this, "edit.alignEast");
         }
-
         public East(DrawingEditor editor, ResourceBundleUtil labels) {
             super(editor);
+            this.labels = labels; // Assign the provided labels
             labels.configureAction(this, "edit.alignEast");
         }
 
         @Override
         protected void alignFigures(Collection<Figure> selectedFigures, Rectangle2D.Double selectionBounds) {
-            double x = selectionBounds.x + selectionBounds.width;
-            for (Figure f : getView().getSelectedFigures()) {
-                if (f.isTransformable()) {
-                    f.willChange();
-                    Rectangle2D.Double b = f.getBounds();
-                    AffineTransform tx = new AffineTransform();
-                    tx.translate(x - b.x - b.width, 0);
-                    f.transform(tx);
-                    f.changed();
-                    fireUndoableEditHappened(new TransformEdit(f, tx));
-                }
+            double rightX = selectionBounds.x + selectionBounds.width;
+            for (Figure figure : selectedFigures) {
+                Rectangle2D.Double figureBounds = figure.getBounds();
+                AffineTransform translateTransform = createTranslateTransform(rightX - figureBounds.x - figureBounds.width, 0);
+                transformFigure(figure, translateTransform);
             }
         }
     }
 
     public static class West extends AlignAction {
-
-        private static final long serialVersionUID = 1L;
-
         public West(DrawingEditor editor) {
             super(editor);
             labels.configureAction(this, "edit.alignWest");
@@ -148,61 +121,44 @@ public abstract class AlignAction extends AbstractSelectedAction {
 
         public West(DrawingEditor editor, ResourceBundleUtil labels) {
             super(editor);
+            this.labels = labels; // Assign the provided labels
             labels.configureAction(this, "edit.alignWest");
         }
 
         @Override
         protected void alignFigures(Collection<Figure> selectedFigures, Rectangle2D.Double selectionBounds) {
-            double x = selectionBounds.x;
-            for (Figure f : getView().getSelectedFigures()) {
-                if (f.isTransformable()) {
-                    f.willChange();
-                    Rectangle2D.Double b = f.getBounds();
-                    AffineTransform tx = new AffineTransform();
-                    tx.translate(x - b.x, 0);
-                    f.transform(tx);
-                    f.changed();
-                    fireUndoableEditHappened(new TransformEdit(f, tx));
-                }
+            double leftX = selectionBounds.x;
+            for (Figure figure : selectedFigures) {
+                Rectangle2D.Double figureBounds = figure.getBounds();
+                AffineTransform translateTransform = createTranslateTransform(leftX - figureBounds.x, 0);
+                transformFigure(figure, translateTransform);
             }
         }
     }
 
     public static class South extends AlignAction {
-
-        private static final long serialVersionUID = 1L;
-
         public South(DrawingEditor editor) {
             super(editor);
             labels.configureAction(this, "edit.alignSouth");
         }
-
         public South(DrawingEditor editor, ResourceBundleUtil labels) {
             super(editor);
+            this.labels = labels; // Assign the provided labels
             labels.configureAction(this, "edit.alignSouth");
         }
 
         @Override
         protected void alignFigures(Collection<Figure> selectedFigures, Rectangle2D.Double selectionBounds) {
-            double y = selectionBounds.y + selectionBounds.height;
-            for (Figure f : getView().getSelectedFigures()) {
-                if (f.isTransformable()) {
-                    f.willChange();
-                    Rectangle2D.Double b = f.getBounds();
-                    AffineTransform tx = new AffineTransform();
-                    tx.translate(0, y - b.y - b.height);
-                    f.transform(tx);
-                    f.changed();
-                    fireUndoableEditHappened(new TransformEdit(f, tx));
-                }
+            double bottomY = selectionBounds.y + selectionBounds.height;
+            for (Figure figure : selectedFigures) {
+                Rectangle2D.Double figureBounds = figure.getBounds();
+                AffineTransform translateTransform = createTranslateTransform(0, bottomY - figureBounds.y - figureBounds.height);
+                transformFigure(figure, translateTransform);
             }
         }
     }
 
     public static class Vertical extends AlignAction {
-
-        private static final long serialVersionUID = 1L;
-
         public Vertical(DrawingEditor editor) {
             super(editor);
             labels.configureAction(this, "edit.alignVertical");
@@ -210,30 +166,22 @@ public abstract class AlignAction extends AbstractSelectedAction {
 
         public Vertical(DrawingEditor editor, ResourceBundleUtil labels) {
             super(editor);
+            this.labels = labels; // Assign the provided labels
             labels.configureAction(this, "edit.alignVertical");
         }
 
         @Override
         protected void alignFigures(Collection<Figure> selectedFigures, Rectangle2D.Double selectionBounds) {
-            double y = selectionBounds.y + selectionBounds.height / 2;
-            for (Figure f : getView().getSelectedFigures()) {
-                if (f.isTransformable()) {
-                    f.willChange();
-                    Rectangle2D.Double b = f.getBounds();
-                    AffineTransform tx = new AffineTransform();
-                    tx.translate(0, y - b.y - b.height / 2);
-                    f.transform(tx);
-                    f.changed();
-                    fireUndoableEditHappened(new TransformEdit(f, tx));
-                }
+            double centerY = selectionBounds.y + (selectionBounds.height / 2);
+            for (Figure figure : selectedFigures) {
+                Rectangle2D.Double figureBounds = figure.getBounds();
+                AffineTransform translateTransform = createTranslateTransform(0, centerY - figureBounds.y - (figureBounds.height / 2));
+                transformFigure(figure, translateTransform);
             }
         }
     }
 
     public static class Horizontal extends AlignAction {
-
-        private static final long serialVersionUID = 1L;
-
         public Horizontal(DrawingEditor editor) {
             super(editor);
             labels.configureAction(this, "edit.alignHorizontal");
@@ -241,22 +189,17 @@ public abstract class AlignAction extends AbstractSelectedAction {
 
         public Horizontal(DrawingEditor editor, ResourceBundleUtil labels) {
             super(editor);
+            this.labels = labels; // Assign the provided labels
             labels.configureAction(this, "edit.alignHorizontal");
         }
 
         @Override
         protected void alignFigures(Collection<Figure> selectedFigures, Rectangle2D.Double selectionBounds) {
-            double x = selectionBounds.x + selectionBounds.width / 2;
-            for (Figure f : getView().getSelectedFigures()) {
-                if (f.isTransformable()) {
-                    f.willChange();
-                    Rectangle2D.Double b = f.getBounds();
-                    AffineTransform tx = new AffineTransform();
-                    tx.translate(x - b.x - b.width / 2, 0);
-                    f.transform(tx);
-                    f.changed();
-                    fireUndoableEditHappened(new TransformEdit(f, tx));
-                }
+            double centerX = selectionBounds.x + (selectionBounds.width / 2);
+            for (Figure figure : selectedFigures) {
+                Rectangle2D.Double figureBounds = figure.getBounds();
+                AffineTransform translateTransform = createTranslateTransform(centerX - figureBounds.x - (figureBounds.width / 2), 0);
+                transformFigure(figure, translateTransform);
             }
         }
     }
