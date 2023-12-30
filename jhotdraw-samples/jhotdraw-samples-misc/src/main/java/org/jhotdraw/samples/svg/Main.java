d97fef56e2b18e15290fa27aa6efdc0996e2b9ce
OthelloEngineer <jolee18@student.sdu.dk>
2023-12-30 17:13:00 +0100

    fixed pom.xml

--- jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/svg/Main.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/svg/Main.java
@@ -6,8 +6,6 @@
  * accompanying license terms.
  */
 package org.jhotdraw.samples.svg;
-
-import dk.sdu.mmmi.featuretracer.lib.FeatureEntryPoint;
 import org.jhotdraw.api.app.Application;
 import org.jhotdraw.app.OSXApplication;
 import org.jhotdraw.app.SDIApplication;
